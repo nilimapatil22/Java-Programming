5cdf4179fccb88e9b533295efddb5c0663e15168
Nilima Yashwant Patil <60321156+Nilima-lab@users.noreply.github.com>
2020-09-04 15:21:00 +0530

    Create array2.java

---
+++ array2.java
@@ -0,0 +1,92 @@
+/*  DESCRIPTION
+Complete the main method to Accept n numbers and display the numbers in ascending order as output ,if n is even. If n is odd, then display the numbers in descending order
+
+Following requirements should be taken care in the program.
+
+
+
+Input should be taken through Console
+Program should print the output as described in the Example Section below
+The first input n should represent the total number of values entered followed by the actual values to be sorted.
+n should be within the range of 1 to 20 . If n is entered as less than 1 or more than 20 , it should show message as INVALID_INPUT.
+Example
+Sample Input 1:
+7
+23 45 67 97 65 34 74 
+
+Expected Output:
+97 74 67 65 45 34 23 
+Sample Input 2:
+6
+77 44 22 65 28 43
+
+Expected Output2:
+22 28 43 44 65 77
+Sample Input 3:
+0
+
+Expected Output 3:
+INVALID_INPUT
+Sample Input 4:
+30
+
+Expected Output 4:
+INVALID_INPUT  */
+
+import java.util.Scanner;
+
+
+/**
+ * Main class
+ */
+public class Source {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=0;
+        int arr[]=new int[20];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        if(n%2==0 && (n>1 || n<20)){
+            
+            for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (arr[i] > arr[j]) 
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        }
+        if(n%2!=0 && (n>1 || n<20)){
+            for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (arr[i] < arr[j]) 
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        }
+        else {
+            System.out.println("INVALID_INPUT");
+        }
+        System.out.println("EXPECTED OUTPUT:");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
