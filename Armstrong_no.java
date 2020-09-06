da1d08f1366d60f7c77401e8276539e6131db6a3
Nilima Yashwant Patil <60321156+Nilima-lab@users.noreply.github.com>
2020-09-06 15:50:12 +0530

    Update Armstrong_no.java

--- Armstrong_no.java
+++ Armstrong_no.java
@@ -1,8 +1,9 @@
+**********************************************************************************************************************************
 /* Complete the main method to find whether a given 3-digit number is an Armstrong number or not.
 
 An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. 
 For example, 371 is an Armstrong number since 33 + 73 + 13 = 371. */
-
+**********************************************************************************************************************************
 
 import java.util.Scanner;
 public class Source {
